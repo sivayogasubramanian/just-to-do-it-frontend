eed6c50d6c8fd645123f4f1d9374a1b6aa850584
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-22 17:37:11 +0800

    abstracting out the elements in the navbar

---
+++ src/components/navbar/items.js
@@ -0,0 +1,8 @@
+import { Inbox, Mail } from '@material-ui/icons';
+
+export const items = [
+  { text: 'Inbox', icon: <Inbox /> },
+  { text: 'Starred', icon: <Mail /> },
+  { text: 'Send email', icon: <Inbox /> },
+  { text: 'Drafts', icon: <Mail /> },
+];
