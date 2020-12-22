00076037dae2c713361457834130a3f89d4f4b94
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-22 17:36:31 +0800

    added route to test navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './routes/login/Login';
 import Register from './routes/login/Register';
+import MiniDrawer from './components/navbar';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/navbar" component={MiniDrawer} />
         </Switch>
       </div>
     </Router>
