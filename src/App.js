d3470e523f10ac5d13acba766e84f7ef2d2e2c1c
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-23 18:30:38 +0800

    added guardedroute to verify user

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GuardedRoute from './helpers/GuardedRoute';
 import Login from './routes/login/Login';
 import Register from './routes/login/Register';
 import MiniDrawer from './components/navigation';
 import Todo from './components/todo';
 import Home from './routes/home';
+import { useState } from 'react';
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/navigation" component={MiniDrawer} />
-          <Route exact path="/todo" component={Todo} />
-          <Route exact path="/home" component={Home} />
+          <GuardedRoute
+            exact
+            path="/navigation"
+            component={MiniDrawer}
+            auth={isAuthenticated}
+          />
+          <GuardedRoute
+            exact
+            path="/todo"
+            component={Todo}
+            auth={isAuthenticated}
+          />
+          <GuardedRoute
+            exact
+            path="/home"
+            component={Home}
+            auth={isAuthenticated}
+          />
           <Route exact path="*" component={Login} />
         </Switch>
       </div>
---
+++ src/helpers/GuardedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const GuardedRoute = ({ component: Component, auth, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (auth ? <Component {...props} /> : <Redirect to="/" />)}
+  />
+);
+
+export default GuardedRoute;
