9b44e9a927d5ecd34c584b1fef8acc6e647a4ad1
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-23 21:53:19 +0800

    added userReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import miscReducer from './miscReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
   isAuthenticated: authReducer,
+  user: userReducer,
   misc: miscReducer,
 });
 
