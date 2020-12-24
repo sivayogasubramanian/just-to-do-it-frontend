73f0083e63688b1c411b5df2274f8f4ba586927e
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-24 20:18:21 +0800

    display todos in home view

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,18 +1,32 @@
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import React from 'react';
+import React, { useEffect } from 'react';
 import MiniDrawer from '../../components/navigation';
 import Todo from '../../components/todo';
 import { useStyles } from './styles';
+import { connect } from 'react-redux';
+import { fetchTodos } from '../../redux/actions/todosActions';
 
-const Home = () => {
+const Home = ({ todos, fetchTodos }) => {
   const classes = useStyles();
+
+  useEffect(() => {
+    fetchTodos();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div>
       <MiniDrawer />
       <div className={classes.toolbar} />
       <div className={classes.content}>
-        <Todo title="hello" completed={true} />
+        {todos.data.map((todo) => (
+          <Todo
+            key={todo.id}
+            title={todo.attributes.title}
+            completed={todo.attributes.completed}
+          />
+        ))}
       </div>
       <Fab
         size="medium"
@@ -26,4 +40,16 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchTodos: () => dispatch(fetchTodos()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
