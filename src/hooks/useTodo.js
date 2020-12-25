f4e3083bfa7be4d78e5de6e0717edfb34cb28c9f
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-25 14:47:57 +0800

    added createTodo method

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -7,21 +7,28 @@ import { fetchTodos } from '../redux/actions/todosActions';
 const useTodo = () => {
   const dispatch = useDispatch();
 
+  const createTodo = () => {
+    authAxios
+      .post(`/api/v1/todos/`, { title: '', completed: false })
+      .then(() => dispatch(fetchTodos()))
+      .catch((error) => Promise.reject(new Error(error)));
+  };
+
   const updateTodo = (todoId, todoData) => {
     authAxios
       .patch(`/api/v1/todos/${todoId}`, todoData)
-      .then(dispatch(fetchTodos()))
-      .catch((error) => Promise.reject(error));
+      .then(() => dispatch(fetchTodos()))
+      .catch((error) => Promise.reject(new Error(error)));
   };
 
   const destroyTodo = (todoId) => {
     authAxios
       .delete(`/api/v1/todos/${todoId}`)
-      .then(dispatch(fetchTodos()))
+      .then(() => dispatch(fetchTodos()))
       .catch((error) => Promise.reject(error));
   };
 
-  return { updateTodo, destroyTodo };
+  return { createTodo, updateTodo, destroyTodo };
 };
 
 export default useTodo;
