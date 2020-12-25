cc9bb9155a2a5815659830429155648ed321af53
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-25 20:55:06 +0800

    added edit functionality (opens dialog)

--- src/components/todo/index.jsx
+++ src/components/todo/index.jsx
@@ -1,6 +1,11 @@
 // React and helpers
 import React, { useState, useRef, useLayoutEffect } from 'react';
 import useTodo from '../../hooks/useTodo';
+import { connect } from 'react-redux';
+// Actions
+import { openDialog } from '../../redux/actions/miscActions';
+// Components
+import EditTodoDialog from '../../components/editTodoDialog';
 // MUI Components
 import {
   Card,
@@ -16,7 +21,14 @@ import DeleteOutlineTwoToneIcon from '@material-ui/icons/DeleteOutlineTwoTone';
 // Styles
 import { useStyles } from './styles';
 
-const Todo = ({ todoId, title, completed }) => {
+const Todo = ({
+  todoId,
+  title,
+  completed,
+  isDialogOpen,
+  dialogTodoId,
+  openDialog,
+}) => {
   const classes = useStyles();
   const { updateTodo, destroyTodo } = useTodo();
   const firstUpdate = useRef(true);
@@ -35,54 +47,74 @@ const Todo = ({ todoId, title, completed }) => {
   const [checked, setChecked] = useState(true);
 
   return (
-    <Slide
-      direction="right"
-      in={checked}
-      mountOnEnter
-      timeout={{ enter: 300, exit: 300 }}
-    >
-      <Card raised className={classes.card}>
-        <Grid
-          container
-          direction="row"
-          justify="space-around"
-          alignItems="center"
-          className={classes.grid}
-        >
-          <Grid item xs={2}>
-            <Checkbox
-              color="primary"
-              checked={isCompleted}
-              onClick={(e) => setIsCompleted(!isCompleted)}
-            />
+    <>
+      <Slide
+        direction="right"
+        in={checked}
+        mountOnEnter
+        timeout={{ enter: 300, exit: 300 }}
+      >
+        <Card raised className={classes.card}>
+          <Grid
+            container
+            direction="row"
+            justify="space-around"
+            alignItems="center"
+            className={classes.grid}
+          >
+            <Grid item xs={2}>
+              <Checkbox
+                color="primary"
+                checked={isCompleted}
+                onClick={(e) => setIsCompleted(!isCompleted)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                value={taskTitle}
+                onChange={(e) => setTaskTitle(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <Button
+                onClick={() =>
+                  openDialog({ isDialogOpen: true, todoId: todoId })
+                }
+                size="large"
+              >
+                <EditTwoToneIcon />
+              </Button>
+            </Grid>
+            <Grid item xs={2}>
+              <Button
+                size="large"
+                onClick={() => {
+                  setChecked(false);
+                  destroyTodo(todoId);
+                }}
+              >
+                <DeleteOutlineTwoToneIcon />
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              value={taskTitle}
-              onChange={(e) => setTaskTitle(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={2}>
-            <Button size="large">
-              <EditTwoToneIcon />
-            </Button>
-          </Grid>
-          <Grid item xs={2}>
-            <Button
-              size="large"
-              onClick={() => {
-                setChecked(false);
-                destroyTodo(todoId);
-              }}
-            >
-              <DeleteOutlineTwoToneIcon />
-            </Button>
-          </Grid>
-        </Grid>
-      </Card>
-    </Slide>
+        </Card>
+      </Slide>
+      {isDialogOpen && <EditTodoDialog />}
+    </>
   );
 };
 
-export default Todo;
+const mapStateToProps = (state) => {
+  return {
+    isDialogOpen: state.misc.dialog.isDialogOpen,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    openDialog: (payload) => dispatch(openDialog(payload)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
