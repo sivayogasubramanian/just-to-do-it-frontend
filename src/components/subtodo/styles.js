02fb8ecc9929e79308a504ec496d5f36fdb60016
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-28 17:23:01 +0800

    removed margin right for edit todo dialog

--- src/components/subtodo/styles.js
+++ src/components/subtodo/styles.js
@@ -4,13 +4,11 @@ export const useStyles = makeStyles({
   card: {
     // Very light Blue
     backgroundColor: 'lightBlue',
-    marginRight: '10px',
     marginTop: '10px',
   },
   cardCompleted: {
     // Very light Blue
     backgroundColor: 'lightGreen',
-    marginRight: '10px',
     marginTop: '10px',
   },
   grid: {
