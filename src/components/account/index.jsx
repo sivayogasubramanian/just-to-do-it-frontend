ce1008c76639d711db627056cf04fce80dcc6fc9
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-01-07 20:26:17 +0800

    added deleteAccount feature and dialog to confirm action

--- src/components/account/index.jsx
+++ src/components/account/index.jsx
@@ -1,7 +1,9 @@
 // React and helpers
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import useAuth from '../../hooks/useAuth';
+// Components
+import DeleteAccountDialog from './DeleteAccountDialog';
 // MUI Components
 import {
   Paper,
@@ -31,106 +33,127 @@ const Account = ({ userId, username, loading, success, errorMsg }) => {
     setPasswordCfm,
     updatePassword,
   } = useAuth();
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     updatePassword(userId, email, oldPassword, password, password_cfm);
   };
 
+  const handleDeleteButton = () => {
+    setIsDeleteDialogOpen(true);
+  };
+
   return (
-    <Slide in={true} direction="down">
-      <Paper elevation={24} className={classes.paper}>
-        <Grid
-          container
-          direction="column"
-          justify="space-around"
-          alignItems="center"
-        >
-          <Typography color="inherit" variant="h4">
-            <b>{username}'s</b> account
-          </Typography>
-          <Typography color="inherit" variant="h5">
-            Change Your Password Here
-          </Typography>
-          <form noValidate onSubmit={handleSubmit}>
-            <TextField
-              variant="outlined"
-              size="medium"
-              margin="normal"
-              fullWidth
-              label="Email"
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></TextField>
-            <TextField
-              variant="outlined"
-              size="medium"
-              margin="normal"
-              fullWidth
-              label="Old Password"
-              type="password"
-              value={oldPassword}
-              onChange={(e) => setOldPassword(e.target.value)}
-            ></TextField>
-            <TextField
-              variant="outlined"
-              size="medium"
-              margin="normal"
-              fullWidth
-              label="New Password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></TextField>
-            <TextField
-              variant="outlined"
-              size="medium"
-              margin="normal"
-              fullWidth
-              label="Confirm New Password"
-              type="password"
-              value={password_cfm}
-              onChange={(e) => setPasswordCfm(e.target.value)}
-            ></TextField>
+    <>
+      <Slide in={true} direction="down">
+        <Paper elevation={24} className={classes.paper}>
+          <Grid
+            container
+            direction="column"
+            justify="space-around"
+            alignItems="center"
+          >
+            <Typography color="inherit" variant="h4">
+              <b>{username}'s</b> account
+            </Typography>
+            <Typography color="inherit" variant="h5">
+              Change Your Password Here
+            </Typography>
+            <form noValidate onSubmit={handleSubmit}>
+              <TextField
+                variant="outlined"
+                size="medium"
+                margin="normal"
+                fullWidth
+                label="Email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></TextField>
+              <TextField
+                variant="outlined"
+                size="medium"
+                margin="normal"
+                fullWidth
+                label="Old Password"
+                type="password"
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
+              ></TextField>
+              <TextField
+                variant="outlined"
+                size="medium"
+                margin="normal"
+                fullWidth
+                label="New Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></TextField>
+              <TextField
+                variant="outlined"
+                size="medium"
+                margin="normal"
+                fullWidth
+                label="Confirm New Password"
+                type="password"
+                value={password_cfm}
+                onChange={(e) => setPasswordCfm(e.target.value)}
+              ></TextField>
+              <Button
+                className={classes.button}
+                variant="contained"
+                size="large"
+                type="submit"
+              >
+                Change Password
+              </Button>
+            </form>
             <Button
               className={classes.button}
               variant="contained"
               size="large"
-              type="submit"
+              color="secondary"
+              onClick={handleDeleteButton}
             >
-              Change Password
+              Delete Account
             </Button>
-          </form>
-        </Grid>
-        {loading && <CircularProgress />}
-        {errorMsg && (
-          <>
-            <ErrorOutlineIcon color="error" fontSize="small" />
-            {errorMsg.error ? (
-              <Typography variant="h6" color="error">
-                Your email and/or old password is wrong. Please Try again.
-              </Typography>
-            ) : (
-              errorMsg.map((error, index) => (
-                <Typography key={index} variant="h6" color="error">
-                  {error}
+          </Grid>
+          {loading && <CircularProgress />}
+          {errorMsg && (
+            <>
+              <ErrorOutlineIcon color="error" fontSize="small" />
+              {errorMsg.error ? (
+                <Typography variant="h6" color="error">
+                  Your email and/or old password is wrong. Please Try again.
                 </Typography>
-              ))
-            )}
-            <br />
-          </>
-        )}
-        {success && (
-          <>
-            <Alert variant="outlined" severity="success" color="success">
-              Password Updated Successfully!
-            </Alert>
-            <br />
-          </>
-        )}
-      </Paper>
-    </Slide>
+              ) : (
+                errorMsg.map((error, index) => (
+                  <Typography key={index} variant="h6" color="error">
+                    {error}
+                  </Typography>
+                ))
+              )}
+              <br />
+            </>
+          )}
+          {success && (
+            <>
+              <Alert variant="outlined" severity="success" color="success">
+                Password Updated Successfully!
+              </Alert>
+              <br />
+            </>
+          )}
+        </Paper>
+      </Slide>
+      <DeleteAccountDialog
+        userId={userId}
+        open={isDeleteDialogOpen}
+        setIsDeleteDialogOpen={setIsDeleteDialogOpen}
+      />
+    </>
   );
 };
 
