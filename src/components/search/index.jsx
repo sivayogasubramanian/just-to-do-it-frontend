1d9b850738ad9f4f9d886af2abcd9b3339aa1eff
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-01-15 16:16:56 +0800

    added search component

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,5 +1,13 @@
 // React and helpers
 import React from 'react';
+import { connect } from 'react-redux';
+// Actions
+import {
+  searchTitle,
+  searchTags,
+  cancelSearch,
+  setSearchView,
+} from '../../redux/actions/searchActions';
 // MUI Components
 import { Card, Grid, Slide } from '@material-ui/core';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -7,13 +15,41 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import SearchBar from 'material-ui-search-bar';
-// MUI Icons
-
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { TextField } from '@material-ui/core';
 // Styles
 import { useStyles } from './styles';
 
-const Search = () => {
+const Search = ({
+  todos,
+  tags,
+  title,
+  currentView,
+  setSearchView,
+  searchTitle,
+  searchTags,
+  cancelSearch,
+}) => {
   const classes = useStyles();
+
+  const allTags = new Set();
+  todos.map((todo) =>
+    todo.attributes.tags.forEach((tag) => {
+      allTags.add(tag);
+    })
+  );
+
+  const handleSelection = (e) => {
+    cancelSearch();
+    setSearchView(e.target.value);
+  };
+  const handleSearchChange = (e) => {
+    searchTitle(e);
+  };
+  const handleCancelSearch = () => cancelSearch();
+  const handleAutocompleteChange = (event, values) => {
+    searchTags(values);
+  };
 
   return (
     <>
@@ -21,7 +57,7 @@ const Search = () => {
         direction="left"
         in={true}
         mountOnEnter
-        timeout={{ enter: 300, exit: 500 }}
+        timeout={{ enter: 200, exit: 200 }}
       >
         <Grid
           container
@@ -30,14 +66,39 @@ const Search = () => {
           alignItems="center"
         >
           <Grid item xs={6}>
-            <Card variant="outlined" className={classes.card}>
-              <SearchBar value={'test'} />
-            </Card>
+            {currentView === 10 ? (
+              <Card variant="outlined" className={classes.card}>
+                <SearchBar
+                  value={title}
+                  onChange={handleSearchChange}
+                  onCancelSearch={handleCancelSearch}
+                />
+              </Card>
+            ) : (
+              <Autocomplete
+                multiple
+                freeSolo
+                autoComplete
+                defaultValue={tags}
+                value={tags}
+                options={[...allTags]}
+                getOptionLabel={(option) => option}
+                onChange={handleAutocompleteChange}
+                filterSelectedOptions
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="outlined"
+                    placeholder="Add Tag"
+                  />
+                )}
+              />
+            )}
           </Grid>
           <Grid item xs={2}>
             <FormControl className={classes.formControl}>
               <InputLabel>Filter</InputLabel>
-              <Select defaultValue={10}>
+              <Select onChange={handleSelection} defaultValue={currentView}>
                 <MenuItem value={10}>By title</MenuItem>
                 <MenuItem value={20}>By tags</MenuItem>
               </Select>
@@ -49,4 +110,22 @@ const Search = () => {
   );
 };
 
-export default Search;
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos.data,
+    currentView: state.search.view,
+    tags: state.search.tags,
+    title: state.search.title,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setSearchView: (view) => dispatch(setSearchView(view)),
+    searchTitle: (title) => dispatch(searchTitle(title)),
+    searchTags: (tags) => dispatch(searchTags(tags)),
+    cancelSearch: () => dispatch(cancelSearch()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
