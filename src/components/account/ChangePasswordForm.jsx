10db83e228a200944e5b4f71833afff98e965de3
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-01-18 00:33:55 +0800

    feat: added autocomplete feat to email textfield in account component

--- src/components/account/ChangePasswordForm.jsx
+++ src/components/account/ChangePasswordForm.jsx
@@ -27,24 +27,29 @@ const ChangePasswordForm = ({ userId }) => {
   };
 
   return (
-    <form noValidate onSubmit={handleSubmit}>
+    <form noValidate onSubmit={handleSubmit} className={classes.form}>
       <TextField
         variant="outlined"
         size="medium"
         margin="normal"
+        id="email"
         fullWidth
-        label="Email"
+        label="Email Address"
+        name="email"
         autoComplete="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       ></TextField>
       <TextField
         variant="outlined"
-        size="medium"
         margin="normal"
         fullWidth
+        name="password"
         label="Old Password"
         type="password"
+        id="password"
+        autoComplete="current-password"
+        size="medium"
         value={oldPassword}
         onChange={(e) => setOldPassword(e.target.value)}
       ></TextField>
