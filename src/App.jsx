01f8c3c628cc645958fd69433b67d2c4bc7c1726
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-01-19 23:02:20 +0800

    feat: added themeprovider context for theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GuardedRoute from './helpers/GuardedRoute';
 import { connect } from 'react-redux';
+import { ThemeProvider } from '@material-ui/core/styles';
 // Routes
 import Landing from './routes/login';
 import Register from './routes/login/Register';
@@ -15,64 +16,69 @@ import MyAccount from './routes/MyAccount';
 import RecentlyDeleted from './routes/RecentlyDeleted';
 // Styles
 import './App.css';
+import light from './theme/light';
+import dark from './theme/dark';
 
-const App = ({ isAuthenticated }) => {
+const App = ({ isAuthenticated, currentTheme }) => {
+  const theme = currentTheme === 'dark' ? dark : light;
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register" component={Register} />
-          <GuardedRoute
-            exact
-            path="/home"
-            component={Home}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute
-            exact
-            path="/home/edit"
-            component={EditTodoDialog}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute
-            exact
-            path="/completed"
-            component={Completed}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute
-            exact
-            path="/today"
-            component={Today}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute
-            exact
-            path="/nextweek"
-            component={NextWeek}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute
-            exact
-            path="/tags"
-            component={Tags}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute
-            exact
-            path="/account"
-            component={MyAccount}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute
-            exact
-            path="/deleted"
-            component={RecentlyDeleted}
-            auth={isAuthenticated}
-          />
-          <GuardedRoute path="/" component={Home} auth={isAuthenticated} />
-        </Switch>
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <GuardedRoute
+              exact
+              path="/home"
+              component={Home}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute
+              exact
+              path="/home/edit"
+              component={EditTodoDialog}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute
+              exact
+              path="/completed"
+              component={Completed}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute
+              exact
+              path="/today"
+              component={Today}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute
+              exact
+              path="/nextweek"
+              component={NextWeek}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute
+              exact
+              path="/tags"
+              component={Tags}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute
+              exact
+              path="/account"
+              component={MyAccount}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute
+              exact
+              path="/deleted"
+              component={RecentlyDeleted}
+              auth={isAuthenticated}
+            />
+            <GuardedRoute path="/" component={Home} auth={isAuthenticated} />
+          </Switch>
+        </ThemeProvider>
       </div>
     </Router>
   );
@@ -81,6 +87,7 @@ const App = ({ isAuthenticated }) => {
 const mapStateToProps = (state) => {
   return {
     isAuthenticated: state.isAuthenticated,
+    currentTheme: state.currentTheme,
   };
 };
 
